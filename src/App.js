d944381c71b6e0573aef340e0384c116f5ba4421
KristianBic <92215832+KristianBic@users.noreply.github.com>
2022-10-14 13:55:58 +0200

    UPDATE: scrollToTop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import Login from "./pages/Login/Login";
 import References from "./pages/References/References";
+import ScrollToTop from "./components/ui/ScrollToTop";
 
 function App() {
 	return (
 		<>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/kontakt" element={<Contact />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/referencie" element={<References />} />
-			</Routes>
+			<ScrollToTop>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/kontakt" element={<Contact />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/referencie" element={<References />} />
+				</Routes>
+			</ScrollToTop>
 		</>
 	);
 }
